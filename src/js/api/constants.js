7a72d1c4aec508b05a79771897945da42ae3752b
Fermain <Fermain@users.noreply.github.com>
2024-08-28 09:58:24 +0200

    Adds API constants

---
+++ src/js/api/constants.js
@@ -0,0 +1,11 @@
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
+
+export const API_KEY = "61e9b0bf-80f9-451e-8787-b876c0610df8";
