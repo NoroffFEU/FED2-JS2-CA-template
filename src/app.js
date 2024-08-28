9f31a7e62f1e7558f42f75216b39bd2f774b041e
Fermain <Fermain@users.noreply.github.com>
2024-08-28 10:01:32 +0200

    Adds logout button listener

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import "./css/style.css";
 
 import router from "./js/router";
+import { setLogoutListener } from "./js/ui/global/logout";
 
 await router(window.location.pathname);
+
+setLogoutListener()
