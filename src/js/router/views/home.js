3398e764298117ff0f20c20ae572ff1aee471e00
Fermain <Fermain@users.noreply.github.com>
2024-08-28 12:49:27 +0200

    Adds simple post template

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,1 +1,12 @@
-alert("This is the root path");
+import { readPosts } from "../../api/post/read";
+import { postTemplate } from "../../templates/post";
+
+if (localStorage.token) {
+  document.querySelector("#content").innerHTML = ""
+
+  const { data: posts, meta } = await readPosts();
+
+  const postTemplates = posts.map(postTemplate);
+
+  document.querySelector("#content").append(...postTemplates)
+}
