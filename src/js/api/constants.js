8e83602276c99234d2881ee1d1f902501ba602d1
Fermain <Fermain@users.noreply.github.com>
2024-08-28 10:19:44 +0200

    Adds social path constants

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -12,4 +12,10 @@ export const API_AUTH_REGISTER = `${API_AUTH}/register`;
 
 export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
 
-export const API_KEY = "61e9b0bf-80f9-451e-8787-b876c0610df8";
+export const API_SOCIAL = `${API_BASE}/social`
+
+export const API_SOCIAL_POSTS = `${API_SOCIAL}/posts`
+
+export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`
+
+export const API_SOCIAL_PROFILE_POSTS = (name) => `${API_SOCIAL_PROFILES}/${name}/posts`;
