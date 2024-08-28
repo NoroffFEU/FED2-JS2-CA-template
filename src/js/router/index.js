9c837c37d20d26c4c4d6aaaa071fe323176401a5
Fermain <Fermain@users.noreply.github.com>
2024-08-28 12:40:02 +0200

    Updates UI and view logic for posts

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -21,6 +21,9 @@ export default async function router(pathname = window.location.pathname) {
     case "/post/edit/":
       await import("./views/postEdit.js");
       break;
+    case "/post/create/":
+      await import("./views/postCreate.js");
+      break;
     case "/profile/":
       await import("./views/profile.js");
       break;
